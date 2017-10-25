f7eafab05c3f96f097286e20e24370140d9fcdc4
yvln <sayyveline@gmail.com>
2017-10-25 13:03:14 -0400

    react auth

---
+++ React_GitHired/githired_app/src/components/Content.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// component that renders content
+// this will render when there is a valid user
+const Content = (props) => {
+  return(
+    <div className="content">
+      <h2>Welcome, {props.user.name}</h2>
+      <p>Your email address is: {props.user.email}</p>
+      <p>You have a cookie set to: {props.user.token}</p>
+      <button onClick={props.logout}>Click here to log out!</button>
+    </div>
+  )
+}
+
+export default Content;
