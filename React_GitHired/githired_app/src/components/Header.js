4b7f268fd9afe51bee4dc5a5a3d90bcc98a5df4c
Rohtas Wadera <rohtaswadera@Rohtass-MacBook-Air.local>
2017-10-26 12:07:13 -0400

    header component to be merged

---
+++ React_GitHired/githired_app/src/components/Header.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+
+
+class Header extends Component {
+
+
+
+
+render() {
+	return (
+
+	<div className = "header"> 
+
+		<div className="username">{this.props.first_name} {this.props.last_name}</div>
+		<div className="logoutBtn"><button onClick={this.props.logout}>Click here to log out!</button></div>
+
+	</div>
+
+		)
+}
+
+}
+
+
+
+
+
+
+export default Header;
