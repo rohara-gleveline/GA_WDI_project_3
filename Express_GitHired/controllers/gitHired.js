c137ab475110e2ee13c7b76ff3542e9fb648af6c
Rohtas Wadera <rohtaswadera@Rohtass-MacBook-Air.local>
2017-10-26 11:19:42 -0400

    express viewall server done

--- Express_GitHired/controllers/gitHired.js
+++ Express_GitHired/controllers/gitHired.js
@@ -1,9 +1,22 @@
 const User = require('../models/user'),
       router = require('express').Router(),
       bcrypt = require('bcryptjs'),
-      Jobs = require('../models/gitHired.js')
+      Jobs = require('../models/gitHired.js');
+
+
 
 // routes go here
+
+
+router.get('/',
+	Jobs.findAll, 
+	(req, res) => {
+		const {jobs} = res.locals
+		res.json({ jobs });
+	});
+
+
+
 
 
 
--- Express_GitHired/models/gitHired.js
+++ Express_GitHired/models/gitHired.js
@@ -1,10 +1,26 @@
 const bcrypt = require('bcryptjs'),
-      db = require('../db/config')
+      db = require('../db/config');
 
-const Jobs = {
+const Jobs = {};
 	// models go here
 
 
-}
+Jobs.findAll = (req, res, next) => {
+	const user_id = req.user.id;
+	db.manyOrNone('SELECT * FROM jobs_data')
+	.then((jobs) => {
+		res.locals.jobs = jobs;
+		console.log('jobs from findAll: ', jobs)
+		next();
+	})
+	.catch(err => {
+		console.log(`Error returning all: ${err}`)
+	})
+};
+
+
+
+
+
 
 module.exports = Jobs;      
