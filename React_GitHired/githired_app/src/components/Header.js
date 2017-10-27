ab120089310ac97c90a40ee8b4516940de595aba
kcontreras01 <kiara@Kiaras-MacBook-Pro.local>
2017-10-27 13:12:28 -0400

    merge

--- React_GitHired/githired_app/src/components/Header.js
+++ React_GitHired/githired_app/src/components/Header.js
@@ -5,7 +5,10 @@ class Header extends Component {
   render() {
     return (
       <div className = "header">
+      <div className="logo">
+        <h3>GitHired</h3>
         <div className="username">{this.props.user.first_name} {this.props.user.last_name}</div>
+        </div>
         <div className="logoutBtn"><button onClick={this.props.logout}>Click here to log out!</button></div>
       </div>
     )
