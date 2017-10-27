c2ee3819cd17e7a86896204239ce2cccf8d083f5
kcontreras01 <kiara@Kiaras-MacBook-Pro.local>
2017-10-27 08:53:39 -0400

    merge

---
+++ React_GitHired/githired_app/src/components/SearchForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Header';
+
+
+const SearchForm = (props) => {
+  return(
+    <div className="searchform">
+      			
+
+    </div>
+  )
+}
+export default SearchForm;
