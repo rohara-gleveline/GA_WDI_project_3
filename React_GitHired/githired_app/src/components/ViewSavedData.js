aaefe14594e302e82dafa642010163130ca16319
Glenn Friedman <glennfriedman@Glenns-MacBook-Pro.local>
2017-10-28 12:10:12 -0400

    updated issue with tablesort crashing page if no data saved, added delete button to savedData table

--- React_GitHired/githired_app/src/components/ViewSavedData.js
+++ React_GitHired/githired_app/src/components/ViewSavedData.js
@@ -10,18 +10,35 @@ class ViewSavedData extends Component {
     super(props);
 
     this.state = {
-      data: []
+      data: [],
+      mode: ''
     }
+    this.onClickDelete = this.onClickDelete.bind(this);
+    this.getSavedData = this.getSavedData.bind(this);
   }
 
   componentDidMount() {
+    this.getSavedData();
+  }
+
+  getSavedData() {
     axios.get(`http://localhost:8080/gitHired/${this.props.user.id}`)
       .then(res => {
         this.setState({
           data: res.data.allJobsData
         }, () => {
+            if(this.state.data.length > 0) {
             tablesort(document.getElementById('myTable'));
+            }
         })
+      })
+  }
+
+  onClickDelete(id) {
+    console.log('clicked');
+    axios.delete(`http://localhost:8080/gitHired/${id}`)
+      .then(res => {
+        this.getSavedData();
       })
   }
 
@@ -57,7 +74,7 @@ class ViewSavedData extends Component {
       		<td><a href={e.url} target='_blank'>Job Link</a></td>
           <td><a href={e.github_jobs_url}><img src="./images/seepage.png"/></a></td>
           <td className="seeMoreButton"> <Link className="linkToViewOne" to={`/ViewOne/${e.id}`}><img src="./images/seemore.png"/></Link></td>
-      		<td className="deleteButton">Delete</td>
+      		<td className="deleteButton" onClick={() => {this.onClickDelete(e.id)}}>Delete</td>
         </tr>
       )
     })
